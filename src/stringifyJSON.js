c04669d95f8e6168fa04336d5687992cb1564c78
Hailey Foster <haileyhasmail@gmail.com>
2015-05-20 11:12:30 -0700

    Begin writing stringifyJSON: -Write base case (numbers, null, boolean and string) -Write first recursive case: array;

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,17 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // Termination Condition if obj is function or undefined
+  // Base case is if item is not an array or object;
+  if (typeof obj === 'number' || obj === null || typeof obj === 'boolean'){
+  	return String(obj);
+  }
+  //Check if item is a string value
+  else if (typeof obj === 'string'){
+  	return '"'+obj+'"';
+  }
+  //Check if item is a number, null, or Boolean
+  //Check if item is undefined
+  // Recursive case for arrays
+  //Recursive case for objects
 };
