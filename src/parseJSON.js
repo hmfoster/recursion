a2e53b99e6c3d64c3953f25dd62d457bb51ab522
Hailey Foster <haileyhasmail@gmail.com>
2015-06-18 16:25:56 -0700

    Parse works on base values and arrays. Begin to write object code.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,18 +4,44 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
-  if (json[0] === "["){
-    var parsed = json.slice(1, json.length -1).split(",");
+  var parsed;
+  
+  //Base values
+  (Number(json) || json === "0") ? parsed = Number(json): parsed = json;
+  json[0] === '"' ? parsed = json.slice(1, json.length -1): parsed = json;
+  if (json === 'true'){
+    parsed = true;
+  } else if (json === 'false'){
+    parsed = false;
+  } else if (json === 'null'){
+    parsed = null;
+  } 
+  
+  //Recursive for arrays
+  else if (json[0] === "[" || json[json.length-1] === "]"){
+    if (json[0] !== "[" || json[json.length-1] !== "]"){
+      throw new SyntaxError('Not parseable');
+    }
+    parsed = json.slice(1, json.length -1).split(",");
     for (var i = 0; i < parsed.length; i++) {
       parsed[i] = parsed[i].trim();
       if (parsed[i].length === 0){
         parsed.splice(i,2);
-      } 
-      else if (parsed[i][0] === "["){
+      } else {
         parsed[i] = parseJSON(parsed[i]);
-      } else if (Number(parsed[i])){
-        parsed[i] = Number(parsed[i]);
       }
     }
+  } 
+  
+  //Recursive for objects
+  else if (json[0] === "{" || json[json.length-1] === "}"){
+    var contents = json.slice(1, json.length -1).split(",").map(function(item){return item.split(":");});
+    parsed = {};
+    for (var i = 0; i < contents.length; i++) {
+      contents[i][0] = contents[i][0].trim();
+      contents[i][1] = contents[i][1].trim();
+      parsed[contents[i][0]] = contents[i][1];
+    };
+
   } return parsed;
-}
+};
